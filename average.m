85ff9586f90a811a73c14a3d042a0ee82cb36d03
Lavin-tom <lavintom007@gmail.com>
2022-10-25 20:05:12 +0530

    added one more code

---
+++ average.m
@@ -0,0 +1,3 @@
+function avge=average(a,b,c)
+    avge=(a+b+c)/3;
+    fprintf(1,'printing in funciton\naverage of %d %d %d is %d\n',a,b,c,avge)
---
+++ avg_of_three_digits_multiple_file.m
@@ -0,0 +1,6 @@
+%avg of three using multiple file compilation
+a=input('enter first digits\n');
+b=input('enter second digits\n');
+c=input('enter third digits\n');
+avge=average(a,b,c);
+fprintf(1,'average of %d %d %d is %d\n',a,b,c,avge)
